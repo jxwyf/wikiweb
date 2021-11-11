e7b68f32eef54f0e5082715d94d27b6ce8f10496
lhalj <2578831517@qq.com>
2021-11-11 16:07:50 +0800

    前端页面布局初始化

---
+++ src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+       <a-layout-footer style="text-align: center">
+      I exist because you need me
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+
+
+</script>
